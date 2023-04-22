2bf967f99e245a3f24f2ba225b5b80f607deb63d
Iordache Ana-Maria <anaiordache264@gmail.com>
2023-04-22 17:22:43 +0300

    functions and filters

--- websites/demo/index.php
+++ websites/demo/index.php
@@ -20,21 +20,43 @@
         [
             'title' => 'Book 1',
             'author' => 'author 1',
+            'releaseYear' => 2008,
             'purchaseUrl' => 'http://example.com'
         ],
         [
             'title' => 'Book 2',
-            'author' => 'author 3',
+            'author' => 'author 2',
+            'releaseYear' => 2018,
+            'purchaseUrl' => 'http://example.com'
+        ],
+        [
+            'title' => 'Book 3',
+            'author' => 'author 2',
+            'releaseYear' => 2020,
             'purchaseUrl' => 'http://example.com'
         ]
     ];
+
+    function filterByAuthor($books, $author) {
+        $filteredBooks = [];
+        foreach($books as $book) {
+            if($book['author'] === $author) {
+                $filteredBooks[] = $book;
+            }
+        }
+
+        return $filteredBooks;
+    }
     ?>
 
     <ul>
-        <?php foreach ($books as $book) : ?>
-            <a href="<?= $book['purchaseUrl'] ?>">
-                <li><?= $book['title'] ?> </li>
-            </a>
+        <?php foreach (filterByAuthor($books, 'author 2') as $book) : ?>
+            
+            <li>
+                <a href="<?= $book['purchaseUrl'] ?>">
+                    <?= $book['title'] ?> (<?= $book['releaseYear'] ?>)
+                </a>
+            </li>
         <?php endforeach; ?>
     </ul>
     
