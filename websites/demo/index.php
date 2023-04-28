814c8f113b67c650043e4c92b0c42f0f44dfcb11
Iordache Ana-Maria <anaiordache264@gmail.com>
2023-04-28 21:09:45 +0300

    page link and php partials

--- websites/demo/index.php
+++ websites/demo/index.php
@@ -1,42 +1,7 @@
 <?php
-       $books = [ 
-        [
-            'title' => 'Book 1',
-            'author' => 'author 1',
-            'releaseYear' => 2008,
-            'purchaseUrl' => 'http://example.com'
-        ],
-        [
-            'title' => 'Book 2',
-            'author' => 'author 2',
-            'releaseYear' => 2018,
-            'purchaseUrl' => 'http://example.com'
-        ],
-        [
-            'title' => 'Book 3',
-            'author' => 'author 2',
-            'releaseYear' => 2020,
-            'purchaseUrl' => 'http://example.com'
-        ]
-    ];
 
-    function filter($items, $fn) {
-        $filteredBooks = [];
-        foreach($items as $item) {
-            if($fn($item)) {
-                $filteredBooks[] = $item;
-            }
-        }
+$heading = "Home";
 
-        return $filteredBooks;
-    }
-
-    $filteredBooks = filter($books, function ($book) { 
-        return $book['releaseYear'] > 2010;
-    });
-
-    // OR we can simply use the php function: array_filter
-
-require 'index.view.php';
+require 'views/index.view.php';
 
 ?> 
---
+++ websites/demo/views/partials/header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head class="h-full bg-gray-100">
+    <meta charset="UTF-8">
+    <title>Demo</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+
+<body class="h-full">
+
+    <div class="min-h-full">
