5f7714fcfcc02117bd50f62f7bc2d5629f2b989f
Iordache Ana-Maria <anaiordache264@gmail.com>
2023-04-29 17:59:11 +0300

    connecting to mysql

--- websites/demo/functions.php
+++ websites/demo/functions.php
@@ -2,7 +2,7 @@
 function dd($value) {
     // if i want to dispaly an array => var_dump(['something')
     echo "<pre>";
-    var_dump($_SERVER); // info about the server - variable that is accessible from any script/file (super global)
+    var_dump($value); // info about the server - variable that is accessible from any script/file (super global)
     echo "</pre>";
 
     die(); // nothing after this will be executed
--- websites/demo/index.php
+++ websites/demo/index.php
@@ -1,4 +1,19 @@
 <?php
     require 'functions.php';
     require 'router.php';
+
+    // connect to MySQL DB
+    // 1. create an instance of PDO class (PHP Data Object)
+    // $dsn -> data durce name => the connection string to the DB
+    $dsn = "mysql:host=localhost;port=3306;dbname=learn_php;charset=utf8mb4";
+    $pdo = new PDO($dsn, 'root', 'anaiordache');
+
+    $statement = $pdo->prepare("select * from posts");
+    $statement->execute();
+
+    $posts = $statement->fetchAll(PDO::FETCH_ASSOC); // to fetch in an associative array rather than indexed
+    
+    foreach($posts as $post) {
+        echo '<li>' . $post['title'] . '</li>';
+    }
 ?>
