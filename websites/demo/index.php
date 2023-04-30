eebc88a6c64fe7c386fc531e27939807036def66
Iordache Ana-Maria <anaiordache264@gmail.com>
2023-04-30 23:40:07 +0300

    create notes table and render the coresp pages

--- websites/demo/index.php
+++ websites/demo/index.php
@@ -1,25 +1,22 @@
 <?php
     require 'functions.php';
+    require 'Database.php'; // moved here so we'll have de db instance before the router is reqired
     require 'router.php';
-    require 'Database.php';
-    
-    $config = require('config.php');
 
-    $db = new Database($config['database']);
-
-    $id = $_GET['id'];
+    // $id = $_GET['id'];
     // $query = "select * from posts where id = {$id}";
     // if i'm writing like this i'm injecting vulerabilitites
     // because the 'id' will take everything that comes after equal in the query
     // so we should binf the variable:
-    $query = "select * from posts where id = ?";
+    // $query = "select * from posts where id = ?";
     // or ':id' instead of '?'
 
     // and here i'll send "['id' => id]"
-    $posts = $db->query($query, [$id])->fetchAll();
+    // $posts = $db->query($query, [$id])->fetchAll();
+    // $posts = $db->query($query)->fetchAll();
 
-    foreach($posts as $post) {
-        echo '<li>' . $post['title'] . '</li>';
-    }
+    // foreach($posts as $post) {
+    //     echo '<li>' . $post['title'] . '</li>';
+    // }
     
 ?>
--- websites/demo/views/partials/nav.php
+++ websites/demo/views/partials/nav.php
@@ -13,6 +13,8 @@
                                     aria-current="page">Home</a>
                                 <a href="/about"
                                     class="<?= isValue('/about' )? 'bg-gray-900 text-white' : 'text-gray-300'?> hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">About</a>
+                                <a href="/notes"
+                                    class="<?= isValue('/notes' )? 'bg-gray-900 text-white' : 'text-gray-300'?> hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Notes</a>
                                 <a href="/contact"
                                     class="<?= isValue('/contact') ? 'bg-gray-900 text-white' : 'text-gray-300'?> hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Contact</a>
                             </div>
