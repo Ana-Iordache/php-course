8b3d5a899e2d12647fd5b7d22b2b491ecef39330
ana_iordache <ana.iordache@acrom.ro>
2023-05-12 16:56:26 +0300

    validator class

---
+++ websites/demo/Validator.php
@@ -0,0 +1,14 @@
+<?php
+class Validator {
+    // this is a pure function => it's not contingent and doesn't depend opon state or values from 
+    // the outside world; so we can make it static -> now we can call it without an instance
+    public static function string($value, $min = 1, $max = INF) {
+        $value = trim($value);
+        return strlen($value) >= $min && strlen($value) <= $max;
+    }
+
+    public static function email($value) {
+        return filter_var($value, FILTER_VALIDATE_EMAIL);
+    }
+}
+?>
